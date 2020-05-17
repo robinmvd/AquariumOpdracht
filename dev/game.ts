1257a26b6ba6a10bb5aa10953f7fd2d81b9b563c
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-05-17 15:58:57 +0200

    Add fish, bubble and pineapple to game.

--- dev/game.ts
+++ dev/game.ts
@@ -2,6 +2,12 @@ class Game {
         
     constructor() {
         console.log("Game was created!")
+
+        let fish : Fish = new Fish()
+
+        let bubble : Bubble = new Bubble()
+
+        let pineapple : Pineapple = new Pineapple()
     }
 } 
 
