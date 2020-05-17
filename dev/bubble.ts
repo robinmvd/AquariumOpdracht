76e17cf658aafcb13c248898c124a63c9cb7bc0f
Robin Michael van Doorn <robinmvdoorn@gmail.com>
2020-05-17 15:57:41 +0200

    Add Bubble class code.

--- dev/bubble.ts
+++ dev/bubble.ts
@@ -1,14 +1,29 @@
 class Bubble {
 
-    div:HTMLElement
+    div: HTMLElement
+    element: HTMLElement
 
     constructor() {
-        console.log("Blub... blub...")
 
-        this.popBubble()
+        console.log("Blub... blub...")
+        this.element = document.createElement("bubble")
+        this.element.addEventListener("click", () => this.popBubble())
+
+        this.changePosition()
+
+        let game = document.getElementsByTagName("game")[0]
+
+        game.appendChild(this.element)
+    }
+
+    changePosition() {
+        let x = Math.random() * (window.innerWidth - this.element.clientWidth)
+        let y = Math.random() * (window.innerHeight - this.element.clientHeight)
+        this.element.style.transform = `translate(${x}px, ${y}px)`
     }
 
     popBubble() {
+        this.element.remove()
         console.log("Plop!")
     }
 }
